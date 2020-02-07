81a2d082b525372740e9c84acfeedfdcf498bf72
Laura MacKew <48992747+l-mack@users.noreply.github.com>
2020-02-06 23:45:32 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,103 @@
+// CAtstroloy app 
+
+// catstrology array 
+
+const catstrologySigns = [
+    {
+        name: 'aries',
+        dateFrom: '21/03',
+        dateTo: '19/04',
+        image: 'aries.png',
+        descript: '',
+    },
+    {
+        name: 'taurus',
+        dateFrom: '20/04',
+        dateTo: '20/05',
+        image: 'taurus.png',
+        descript: '',
+    },
+    {
+        name: 'gemini',
+        dateFrom: '21/05',
+        dateTo: '20/06',
+        image: 'gemini.png',
+        descript: '',
+    },
+    {
+        name: 'cancer',
+        dateFrom: '21/06',
+        dateTo: '22/07',
+        image: 'cancer.png', 
+        descript: '',
+    },
+    {
+        name: 'leo',
+        dateFrom: '07/23',
+        dateTo: '22/08',
+        image: 'leo.png',
+        descript: '',
+    },
+    {
+        name: 'virgo',
+        dateFrom: '23/08',
+        dateTo: '22/09',
+        image: 'virgo.png',
+        descript: '',
+    },
+    {
+        name: 'libra',
+        dateFrom: '23/09',
+        dateTo: '22/10',
+        image: 'libra.png',
+        descript: '',
+    },
+    {
+        name: 'scorpio',
+        dateFrom: '23/10',
+        dateTo: '21/11',
+        image: 'scorpio.png',
+        descript: '',
+    },
+    {
+        name: 'sagittarius',
+        dateFrom: '22/11',
+        dateTo: '21/12',
+        image: 'sagittarius.png',
+        descript: '',
+    },
+    {
+        name: 'capricorn',
+        dateFrom: '22/12',
+        dateTo: '19/01',
+        image: 'capricorn.png',
+        descript: '',
+    },
+    {
+        name: 'aquarius',
+        dateFrom: '20/01',
+        dateTo: '18/02',
+        image: 'aquarius.png',
+        descript: '',
+    },
+    {
+        name: 'pisces',
+        dateFrom: '19/02',
+        dateTo: '20/03',
+        image: 'pisces.png',
+        descript: '',
+    },
+
+];
+
+
+// Bind event listener to form submission event 
+
+
+// capture user input in a variable
+
+
+// if inputDate is > dateFrom && < dateTo
+
+
+// Loop across castrology array and display object name, img and description 
