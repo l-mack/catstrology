bde221a9bd2f334ae6fef98ab1972b09e62a6b9c
Laura MacKew <48992747+l-mack@users.noreply.github.com>
2020-02-13 15:37:32 -0500

    resopnsivity added, mvp reached, working

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
                 endDate: '119',
                 image: 'capricorn.png',
                 imageCat: 'capricornCat.png',
-                descript: 'Hey it’s your  boss, welcome to their world, hope you like serving them. Persistent to the core, if they want something: They. Will. Have. It. Be it a door opened late at night or attention right here right now, it will be theirs. Loving and loyal you’ll enjoy their rein over you.',
+                descript: 'Hey it’s your boss, welcome to their world, hope you like serving them. Persistent to the core, if they want something: They. Will. Have. It. Be it a door opened late at night or attention right here right now, it will be theirs. Loving and loyal you’ll enjoy their rein over you.',
                 attribution: 'Capricorn Cat by Denis Sazhin from the Noun Project',
             },
             {
@@ -22,7 +22,7 @@
                 endDate: '218',
                 image: 'aquarius.png',
                 imageCat: 'aquariusCat.png',
-                descript: 'something',
+                descript: 'Aquarian cats can’t be normal.  Even for cats, these creatures are weird.  They probably have you face-palming on the regular, saying “did you really just eat that” or “are you really sleeping like that?” Redefining what it means to be “quirky” Aquarian felines are always full of surprises. Keep their active minds busy with lots of games and play time. Teach them to fetch, if any cat is likely to pick it up, it’s an aqua-kitty.',
                 attribution: 'Aquarius Cat by Denis Sazhin from the Noun Project',
             },
             {
@@ -32,7 +32,7 @@
                 endDate: '320',
                 image: 'pisces.png',
                 imageCat: 'piscesCat.png',
-                descript: 'something',
+                descript: 'This is the cat that seems to know when you’re upset and will come lay on your lap.  Pieces cats have a calm demeanour, not demanding too much attention their active imagination keeps them entertained. They can be found dreaming or otherwise in their own world, clearly existing in another dimension.',
                 attribution: 'Pisces Cat by Denis Sazhin from the Noun Project',
             },
             {
@@ -42,7 +42,7 @@
                 endDate: '419',
                 image: 'aries.png',
                 imageCat: 'ariesCat.png',
-                descript: 'Always hangry, these bossy busy bodies are always moving.  Their fiery energy makes them avid hunters, probably leaving lots of “gifts” for you.  Be sure to give them lots of play time and maybe they’ll think about snuggling for a bit. ',
+                descript: 'Always hangry, these bossy busy bodies are always moving.  Their fiery energy makes them avid hunters, and I’m betting they leave lots of “gifts” for you.  Be sure to give them lots of play time and maybe they’ll think about snuggling you for a bit.',
                 attribution: 'Aries Cat by Denis Sazhin from the Noun Project',
             },
             {
@@ -52,7 +52,7 @@
                 endDate: '520',
                 image: 'taurus.png',
                 imageCat: 'taurusCat.png',
-                descript: 'Feeding time is all the time for these stalky fur balls. They love their food and tastes treats. Avid nappers and often enjoy long luxurious pets, except when they don’t, and they be sure to let you know.',
+                descript: 'Feeding time is all the time for these stalky fur balls. They love food and tasty treats. Avid nappers and often enjoy long luxurious pets, except when they don’t, and they be sure to let you know.',
                 attribution: 'Taurus Cat by Denis Sazhin from the Noun Project'
             },
             {
@@ -62,7 +62,7 @@
                 endDate: '620',
                 image: 'gemini.png',
                 imageCat: 'geminiCat.png',
-                descript: 'Talkitty talk talk Gemini cats have a lot to say. A meow for every mood. And energy for days. Their Bizaar behaviour probably keeps you guessing. Chasing shadows, stealing from your purse, playing games at 4am, never a dull day with a Gemini fur baby.',
+                descript: 'Talk-itty talk talk Gemini cats have a lot to say. A meow for every mood and energy for days. Their bizarre behaviour probably keeps you guessing, chasing shadows, stealing from your purse, playing games at 4am… Never a dull day with a Gemini fur baby!',
                 attribution: 'Gemini Cat by Denis Sazhin from the Noun Project',
             },
             {
@@ -72,7 +72,7 @@
                 endDate: '722',
                 image: 'cancer.png', 
                 imageCat: 'cancerCat.png',
-                descript: 'The hide and seekers, cancer cats like to find weird little hide away spot for peace and quite times. Very affectionate and loving to their family, they can be very protective and shy around new comers. Let them come to you when their ready.',
+                descript: 'Hide and seekers of the kitty zodiac, cancer cats like to find little hide away spots for peace and quiet.  Very affectionate and loving to their family.  Easily overwhelmed they can be timid and shy around new comers. Let them come to you when they’re ready.',
                 attribution: 'Cancer Cat by Denis Sazhin from the Noun Project',
 
             },
@@ -83,7 +83,7 @@
                 endDate: '822',
                 image: 'leo.png',
                 imageCat: 'leoCat.png',
-                descript: '“What are you doing and why aren’t you paying attention to me???” Leo cats are the centre of their own universe, and they’re assuming the centre of yours too. They live for affection and attention and if they don’t get it, you’ll be sure to hear about it, maybe they’ll shred the toilet paper or leave a “gift” on your bed.',
+                descript: '“What are you doing and why aren’t you paying attention to me???” Leo cats are the centre of their own universe, and they’re assuming the centre of yours’s too. They live for affection and attention and if they don’t get it, you’ll be sure to hear about it (maybe they’ll shred the toilet paper or leave a “gift” on your bed).',
                 attribution: 'Leo Cat by Denis Sazhin from the Noun Project',
             },
             {
@@ -93,8 +93,7 @@
                 endDate: '922',
                 image: 'virgo.png',
                 imageCat: 'virgoCat.png',
-                descript: 'Picky and particular, Virgo cats like what they like and don’t what they don’t. Probably have preferences for clean food bowels and litters, but in turn are tidy cats to take care of. Once you find the food they like, best to just stick with it for both of your respective sanity’s sake.',
-                attribution: 'Virgo Cat by Denis Sazhin from the Noun Project',
+                descript: 'Picky and particular, Virgo cats like what they like and don’t what they don’t. They probably have preference for very clean food bowels and litters, but are tidy cats to take care of in turn. Once you find the food they like, best to just stick with it for both of your respective sanity’s sake. Avid groomers they take a lot of care in keeping clean and neat.',
             },
             {
                 name: 'libra',
@@ -103,7 +102,7 @@
                 endDate: '1022',
                 image: 'libra.png',
                 imageCat: 'libraCat.png',
-                descript: 'The socialites of kitty land, Libra cats do best with a kitty friend in their home to keep them company.  Never shy Libra kitties wanna know you, and your friends too. Bouncing from lap to lap for all the attention and affection a room has to offer.',
+                descript: 'The socialites of kitty land, Libra cats do best with a kitty friend in their home to keep them company.  Never shy Libra kitties wanna know you, and your friends too, bouncing from lap to lap for all the attention and affection a room has to offer.',
                 attribution: 'Libra Cat by Denis Sazhin from the Noun Project',
             },
             {
@@ -113,7 +112,7 @@
                 endDate: '1122',
                 image: 'scorpio.png',
                 imageCat: 'scorpioCat.png',
-                descript: 'These mysterious and intense felines are mood af. They will not come when you call, might rub on your leg one minute and turn around and bite your ankle the next. They are known to get jealous if you’re found giving affection to another cat so don’t turn your back on these fiesty fur demons, and sleep with one eye open.',
+                descript: 'These mysterious and intense felines are mood AF. They will not come when you call, might rub on your leg one minute and turn around and bite your ankle the next. They are known to get jealous if you’re found giving affection to another cat so don’t turn your back on these feisty fur demons… maybe sleep with one eye open.',
                 attribution: 'Scorpio Cat by Denis Sazhin from the Noun Project',
             },
             {
@@ -123,7 +122,7 @@
                 endDate: '1221',
                 image: 'sagittarius.png',
                 imageCat: 'sagittariusCat.png',
-                descript: 'Wanderers and explorers, Sagittarius kitties are on the move. Endlessly curious, these cats will find get into every nook and cranny trying to find excitement. Sag cats DGAF, reckless, they’ve been known to nock overs valuables in pursuit of an adventure.',
+                descript: 'Wanderers and explorers, Sagittarius kitties are on the move. Endlessly curious, these cats will get into every nook and cranny looking for excitement. A little reckless, they’ve been known to nock overs valuables in pursuit of an adventure.',
                 attribution: 'Sagittarius Cat by Denis Sazhin from the Noun Project',
             },
             {
@@ -133,7 +132,7 @@
                 endDate: '1231',
                 image: 'capricorn.png',
                 imageCat: 'capricornCat.png',
-                descript: 'Hey it’s your  boss, welcome to their world, hope you like serving them. Persistent to the core, if they want something: They. Will. Have. It. Be it a door opened late at night or attention right here right now, it will be theirs. Loving and loyal you’ll enjoy their rein over you.',
+                descript: 'HHey it’s your boss, welcome to their world, hope you like serving them. Persistent to the core, if they want something: They. Will. Have. It. Be it a door opened late at night or attention right here right now, it will be theirs. Loving and loyal you’ll enjoy their rein over you. ',
                 attribution: 'Capricorn Cat by Denis Sazhin from the Noun Project',
             }
         ];
@@ -153,7 +152,7 @@
 
     // compare userInput to signs array
         const result = catstrology.signs.find(sign => sign.endDate >= userInput);
-        console.log(result);
+        // console.log(result);
 
         const signInfo = Object.values(result);
         const htmlToAppend = `
@@ -187,10 +186,9 @@
 
         catstrology.events();    
 
-        };
+    };
 
 // document ready 
     $(function() {
         catstrology.init();
-
 });
